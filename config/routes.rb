0a93fe2fab7a18c358afcc076b69cda1ba145215
ttm-mk <129599412+ttm-mk@users.noreply.github.com>
2023-04-14 01:36:05 +0000

    [Finish]4-13

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'about'
   devise_for :users
   
-  resources :post_images, only: [:new, :create, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   
 end
